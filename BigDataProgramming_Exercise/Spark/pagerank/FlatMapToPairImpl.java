3845c8d31c26c655c98e3d34e485418ba64d2014
CelesteXiong <55577483+CelesteXiong@users.noreply.github.com>
2019-10-30 16:40:47 +0800

    Add files via upload

--- BigDataProgramming_Exercise/Spark/pagerank/FlatMapToPairImpl.java
+++ BigDataProgramming_Exercise/Spark/pagerank/FlatMapToPairImpl.java
@@ -1,5 +1,6 @@
 package DSPPCode.spark.pagerank;
 
+import org.spark_project.guava.collect.Iterables;
 import scala.Tuple2;
 
 import java.util.Iterator;
@@ -10,10 +11,11 @@ public class FlatMapToPairImpl extends FlatMapToPair{
     public Iterator<Tuple2<String, Double>> call(Tuple2<Iterable<String>, Double> outsideWeight) throws Exception {
         List<Tuple2<String, Double>> output = new ArrayList<>();
         int num = 0;
-        for (String Id : outsideWeight._1)
-        {
-            num+=1;
-        }
+//        for (String Id : outsideWeight._1)
+//        {
+//            num+=1;
+//        }
+        num = Iterables.size(outsideWeight._1);
         for (String Id : outsideWeight._1)
         {
             output.add(new Tuple2(Id,outsideWeight._2/num));
